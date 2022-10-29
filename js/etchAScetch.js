026d27db4709011e87a51d9d87b8d72a1b404c45
Henrik Breimark <henrik.breimark@gmail.com>
2022-10-29 17:08:17 +0200

    Will add a basic layput with a input button that lets the user set the size of the grid from 1-100 cells.

--- js/etchAScetch.js
+++ js/etchAScetch.js
@@ -1,35 +1,42 @@
 let drawingBoard = document.getElementById('board');
-let resetBoardButton = document.getElementById('resetBoard')
-
+let resetBoardButton = document.getElementById('resetBoard');
+let squareBox = document.getElementById('squares');
+let firstLoad = true;
 
 addEventListener('load', buildGrid);
 
-
 resetBoardButton.addEventListener('click', resetBoard);
 
-
-
-function buildGrid(){
+function buildGrid() {
     let newBoard = document.createElement('div');
-    newBoard.classList.add('boardLayout')
-    drawingBoard.appendChild(newBoard);
+    let inputValue = squareBox.value;
+    let numberOfSquares = parseInt(inputValue);
 
-    for (let index = 0; index < 2500; index++) {
+    if (isNaN(inputValue)) {
+        numberOfSquares = 1;
+    }
+    if (numberOfSquares > 100) {
+        numberOfSquares = 1;
+    }
+    newBoard.classList.add('boardLayout');
+    newBoard.style.gridTemplateColumns = `repeat(${numberOfSquares}, 1fr)`;
+    drawingBoard.appendChild(newBoard);
+    for (let index = 0; index < (numberOfSquares * numberOfSquares); index++) {
         let newPixel = document.createElement('div');
         newPixel.classList.add('pixel');
-        newBoard.appendChild(newPixel); 
-        newPixel.addEventListener('pointerover', changeOpacity)
+        newBoard.appendChild(newPixel);
+        newPixel.addEventListener('pointerover', changeOpacity);
     }
+
 }
 
-function changeOpacity (evt){
+function changeOpacity(evt) {
     let target = evt.target;
     let currentOpacity = getComputedStyle(target).getPropertyValue("opacity");
     target.style.opacity = parseFloat(currentOpacity) + 0.1;
 }
 
-
-function resetBoard (){
+function resetBoard() {
     let currentBoard = document.querySelector('.boardLayout');
     currentBoard.remove();
     buildGrid();
